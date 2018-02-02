5f6bfbd08c60f43eab4513990807e2ca0a0af474
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2018-02-02 22:20:45 +0100

    use fs store instead of fs store 2

--- studio/main.js
+++ studio/main.js
@@ -59,7 +59,7 @@
 	function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 	
 	_jsreportStudio2.default.initializeListeners.push(function () {
-	  if (!_jsreportStudio2.default.extensions['fs-store-2'].options.syncModifications) {
+	  if (!_jsreportStudio2.default.extensions['fs-store'].options.syncModifications) {
 	    console.log('Skipping active sync with server');
 	    return;
 	  }
