b0b0bd95bff359e19e12452c40058a2acb914c72
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2018-02-27 15:43:16 +0100

    use common core tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const DocumentStore = require('jsreport-core/lib/store/documentStore.js')
+const coreStoreTests = require('jsreport-core').tests.documentStore
 const Provider = require('../lib/provider')
 const path = require('path')
 const Promise = require('bluebird')
@@ -72,34 +73,11 @@ describe('provider', () => {
     return rimrafAsync(tmpData)
   })
 
+  describe('common core tests', () => {
+    coreStoreTests()(() => store)
+  })
+
   describe('basic', () => {
-    it('insert and query', async () => {
-      await store.collection('templates').insert({ name: 'test' })
-      const res = await store.collection('templates').find({ name: 'test' })
-      res.length.should.be.eql(1)
-    })
-
-    it('insert and query with condition', async () => {
-      await store.collection('templates').insert({ name: 'test' })
-      const res = await store.collection('templates').find({ name: 'diferent' })
-      res.length.should.be.eql(0)
-    })
-
-    it('insert, update, query', async () => {
-      await store.collection('templates').insert({ name: 'test' })
-      await store.collection('templates').update({ name: 'test' }, { $set: { recipe: 'foo' } })
-      const res = await store.collection('templates').find({ name: 'test' })
-      res.length.should.be.eql(1)
-      res[0].recipe.should.be.eql('foo')
-    })
-
-    it('insert remove query', async () => {
-      await store.collection('templates').insert({ name: 'test' })
-      await store.collection('templates').remove({ name: 'test' })
-      const res = await store.collection('templates').find({ name: 'test' })
-      res.length.should.be.eql(0)
-    })
-
     it('remove should delete doc folder', async () => {
       await store.collection('templates').insert({ name: 'test' })
       fs.existsSync(path.join(tmpData, 'templates', 'test')).should.be.true()
@@ -107,39 +85,11 @@ describe('provider', () => {
       fs.existsSync(path.join(tmpData, 'templates', 'test')).should.be.false()
     })
 
-    it('insert should return an object with _id set', async () => {
-      const doc = await store.collection('templates').insert({ name: 'test' })
-      doc.should.have.property('_id')
-      doc._id.should.be.ok()
-    })
-
     it('insert, update to a different name', async () => {
       await store.collection('templates').insert({ name: 'test' })
       await store.collection('templates').update({ name: 'test' }, { $set: { name: 'test2' } })
       const res = await store.collection('templates').find({ name: 'test2' })
       res.length.should.be.eql(1)
-    })
-
-    it('update with upsert', async () => {
-      await store.collection('templates').update({ name: 'test' }, { $set: { name: 'test2' } }, { upsert: true })
-      const res = await store.collection('templates').find({ name: 'test2' })
-      res.length.should.be.eql(1)
-    })
-
-    it('find should return clones', async () => {
-      await store.collection('templates').insert({ name: 'test', content: 'original' })
-      const res = await store.collection('templates').find({})
-      res[0].content = 'modified'
-      const res2 = await store.collection('templates').find({})
-      res2[0].content.should.be.eql('original')
-    })
-
-    it('insert should use clones', async () => {
-      const doc = { name: 'test', content: 'original' }
-      await store.collection('templates').insert(doc)
-      doc.content = 'modified'
-      const res = await store.collection('templates').find({})
-      res[0].content.should.be.eql('original')
     })
   })
 
@@ -195,52 +145,6 @@ describe('provider', () => {
 
       const res = await store.collection('templates').find({})
       res.should.have.length(1)
-    })
-  })
-
-  describe('queries', () => {
-    it('skip and limit', async () => {
-      await store.collection('templates').insert({ name: '1' })
-      await store.collection('templates').insert({ name: '3' })
-      await store.collection('templates').insert({ name: '2' })
-
-      const res = await store.collection('templates').find({}).skip(1).limit(1).sort({name: 1}).toArray()
-      res.length.should.be.eql(1)
-      res[0].name.should.be.eql('2')
-    })
-
-    it('$and', async () => {
-      await store.collection('templates').insert({ name: '1', recipe: 'a' })
-      await store.collection('templates').insert({ name: '2', recipe: 'b' })
-      await store.collection('templates').insert({ name: '3', recipe: 'b' })
-
-      const res = await store.collection('templates').find({$and: [{name: '2'}, {recipe: 'b'}]}).toArray()
-      res.length.should.be.eql(1)
-      res[0].name.should.be.eql('2')
-      res[0].recipe.should.be.eql('b')
-    })
-
-    it('projection', async () => {
-      await store.collection('templates').insert({ name: '1', recipe: 'a' })
-
-      const res = await store.collection('templates').find({}, { recipe: 1 })
-      res.length.should.be.eql(1)
-      res[0].should.not.have.property('name')
-      res[0].recipe.should.be.eql('a')
-    })
-
-    it('count', async () => {
-      await store.collection('templates').insert({ name: '1', recipe: 'a' })
-
-      const res = await store.collection('templates').find({}).count()
-      res.should.be.eql(1)
-    })
-
-    it('count without cursor', async () => {
-      await store.collection('templates').insert({ name: '1', recipe: 'a' })
-
-      const res = await store.collection('templates').count({})
-      res.should.be.eql(1)
     })
   })
 
